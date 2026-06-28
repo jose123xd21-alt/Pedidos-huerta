b7f65f3bf2bea2aa373c73253f036c153017c5e6
jose123xd21-alt <jose123xd21@gmail.com>
2026-06-28 18:11:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,37 @@
-const CACHE_NAME = 'huerto-tpv-pro-v30';
+const CACHE_NAME = 'huerto-tpv-pro-v31';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './firebase-config.js',
+  './manifest.json',
+  './assets/icon-192.png',
+  './assets/icon-512.png'
+];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(() => null))
+  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => response)
+      .catch(() => caches.match(event.request))
+  );
 });
